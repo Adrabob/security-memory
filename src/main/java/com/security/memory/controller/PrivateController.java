7b8dd897294ba1a3e4e50e38e8e8ce1a23b8717a
Adrabob <arda_kaya.fb@hotmail.com>
2024-04-16 09:56:45 +0300

    hasRole Updated

--- src/main/java/com/security/memory/controller/PrivateController.java
+++ src/main/java/com/security/memory/controller/PrivateController.java
@@ -1,5 +1,6 @@
 package com.security.memory.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,15 @@ public class PrivateController {
 	@GetMapping
 	public String helloWorldPrivate() {
 		return "Hello World from private endpoint";
+	}
+	
+	@GetMapping("/user")
+	public String helloWorldUserPrivate() {
+		return "Hello World from user private endpoint";
+	}
+	
+	@GetMapping("/admin")
+	public String helloWorldAdminPrivate() {
+		return "Hello World from admin private endpoint";
 	}
 }
